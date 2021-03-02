1cc245b37c02bca994759163ff3b4a04d121b21e
Marharyta-Tkachenko <margoshyn23@gmail.com>
2021-03-02 14:14:04 +0200

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Student\ Tkachenko.Marharyta.IK11\Lab1.3\Lab1.3\Complex.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Complex c, d;
+			c.Init(4, 8);
+			d.Init(4, 8);
+			bool t = Equ(c, d);
+			Assert::AreEqual(t, true);
+		}
+	};
+}
